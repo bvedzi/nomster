17b831376d6fef6d9e2bd08f55d733695e5b47ad
Burim <vdzvsk@gmail.com>
2019-09-17 18:09:40 +0000

    update 9-17

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,5 +2,18 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def new
+    @place = Place.new
+  end
 end
 
+def create
+  Place.create(place_params)
+end
+
+private
+
+def place_params
+  params.require(:place).permit(:name, :description, :address)
+end
