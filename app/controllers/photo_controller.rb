ef384dcaa39a73f65610929d1747354223eb3a80
Burim <vdzvsk@gmail.com>
2019-10-03 23:56:47 +0000

    lesson 39.4

--- app/controllers/photo_controller.rb
+++ app/controllers/photo_controller.rb
@@ -2,7 +2,7 @@ class PhotoController < ApplicationController
   before_action :authenticate_user!
   def create
     @place = Place.find(params[:place_id])
-    @place.photo.create(photo_params.merge(user: current_user))
+    @place.photos.create(photo_params.merge(user: current_user))
     redirect_to place_path(@place)
   end
 
